cd42fa90359a86b6edb4aeebcf4de0e2864aacbc
31ayush05 <52788335+31ayush05@users.noreply.github.com>
2023-10-07 13:29:35 +0530

    gradient difficulty

--- pathFinder.py
+++ pathFinder.py
@@ -68,127 +68,144 @@ def makeMaze(cv,mX,mY):
     pCent = 0
     curr = 0
 
+    solution = []  # array to store the solution path
+
     while(len(src) > 0):
         # begin looping for each source's each possible pathway
         #      after every pathway production add new sources
         #      a source dies if all its neghboring cells are visited
 
-        for s in src:
-            # here s consists of arrays of points that are in the path
-            pathHead = s[-1]
-            # get all possible paths
-            paths = getPossiblePaths(pathHead,visited)
-            # foreach path 50-50 chance for it to be made or not
+        for k in range(len(src)):
+            # getting the actual path in s
+            s = src[k]
 
-            if len(paths) > 0:
-                farPaths = []
-                # fill the farPaths Array
-                for path in paths:
-                    # calculate displacement for each path
-                    nxtCell = []
-                    if path == "l":
-                        nxtCell = [pathHead[0]-1,pathHead[1]]
-                    elif path == "r":
-                        nxtCell = [pathHead[0]+1,pathHead[1]]
-                    elif path == "t":
-                        nxtCell = [pathHead[0],pathHead[1]-1]
-                    else:
-                        nxtCell = [pathHead[0],pathHead[1]+1]
+            # initial PathLooping
+            j = 10
+            if k == 0:
+                j = 20
+
+            # LOOP
+            for iPl in range(j):
+                # here s consists of arrays of points that are in the path
+                pathHead = s[-1]
+                # get all possible paths
+                paths = getPossiblePaths(pathHead,visited)
+                # foreach path 50-50 chance for it to be made or not
+
+                if len(paths) > 0:
+                    farPaths = []
+                    # fill the farPaths Array
+                    for path in paths:
+                        # calculate displacement for each path
+                        nxtCell = []
+                        if path == "l":
+                            nxtCell = [pathHead[0]-1,pathHead[1]]
+                        elif path == "r":
+                            nxtCell = [pathHead[0]+1,pathHead[1]]
+                        elif path == "t":
+                            nxtCell = [pathHead[0],pathHead[1]-1]
+                        else:
+                            nxtCell = [pathHead[0],pathHead[1]+1]
+                        
+                        disp = len(s)/((nxtCell[0]**2 + nxtCell[1]**2)**0.5)
+
+                        farPaths.append([path,disp])
+
+                    # sort the array
+                    sortedArr = []
+                    while len(farPaths) > 0:
+                        a = []
+                        for fP in farPaths:
+                            a.append(fP[1])
+                        a = a.index(max(a))
+                        sortedArr.append(farPaths[a])
+                        farPaths.pop(a)
                     
-                    disp = len(s)/((nxtCell[0]**2 + nxtCell[1]**2)**0.5)
+                    farPaths.extend(sortedArr)
+                    sortedArr.clear()
 
-                    farPaths.append([path,disp])
-
-                # sort the array
-                sortedArr = []
-                while len(farPaths) > 0:
-                    a = []
-                    for fP in farPaths:
-                        a.append(fP[1])
-                    a = a.index(max(a))
-                    sortedArr.append(farPaths[a])
-                    farPaths.pop(a)
-                
-                farPaths.extend(sortedArr)
-                sortedArr.clear()
-
-                # make probability distribution
-                pDist = []
-                # if 1 path = [1]
-                # if 2 path = [0.8,0.2]
-                # if 3 path = [0.7,0.2,0.1]
-                if len(farPaths) == 3:
-                    pDist = [10,10,10]
-                elif len(farPaths) == 2:
-                    pDist = [10,10]
-                else:
-                    pDist = [6]
-                
-                # choose a path using random randint
-                rand = random.randint(0,9)
-                chosenPath = ""
-                if len(pDist) == 1:
-                    if rand < pDist[0]:
-                        chosenPath = farPaths[0][0]
-                elif len(pDist) == 2:
-                    if rand < pDist[0]:
-                        chosenPath = farPaths[0][0]
-                    elif rand < pDist[1]:
-                        chosenPath = farPaths[1][0]
+                    # make probability distribution
+                    pDist = []
+                    # if 1 path = [1]
+                    # if 2 path = [0.8,0.2]
+                    # if 3 path = [0.7,0.2,0.1]
+                    if len(farPaths) == 3:
+                        pDist = [7,9,10]
+                    elif len(farPaths) == 2:
+                        pDist = [8,10]
                     else:
-                        pass
-                else:
-                    if rand < pDist[0]:
-                        chosenPath = farPaths[0][0]
-                    elif rand < pDist[1]:
-                        chosenPath = farPaths[1][0]
-                    elif rand < pDist[2]:
-                        chosenPath = farPaths[2][0]
+                        pDist = [6]
+                    
+                    # choose a path using random randint
+                    rand = random.randint(0,9)
+                    chosenPath = ""
+                    if len(pDist) == 1:
+                        if rand < pDist[0]:
+                            chosenPath = farPaths[0][0]
+                    elif len(pDist) == 2:
+                        if rand < pDist[0]:
+                            chosenPath = farPaths[0][0]
+                        elif rand < pDist[1]:
+                            chosenPath = farPaths[1][0]
+                        else:
+                            pass
                     else:
-                        pass
-                
-                # implement the path chosen
-                nxtCell = []
-                if (len(chosenPath) > 0) and shouldRun():
-                    if chosenPath == "l":
-                        arr[pathHead[1]][pathHead[0]] += "l"
-                        nxtCell = [pathHead[0]-1,pathHead[1]]
+                        if rand < pDist[0]:
+                            chosenPath = farPaths[0][0]
+                        elif rand < pDist[1]:
+                            chosenPath = farPaths[1][0]
+                        elif rand < pDist[2]:
+                            chosenPath = farPaths[2][0]
+                        else:
+                            pass
+                    
+                    # implement the path chosen
+                    nxtCell = []
+                    if (len(chosenPath) > 0) and shouldRun():
+                        if chosenPath == "l":
+                            arr[pathHead[1]][pathHead[0]] += "l"
+                            nxtCell = [pathHead[0]-1,pathHead[1]]
 
-                        # including refrence to parent
-                        arr[pathHead[1]][pathHead[0]-1] += "r"
-                    elif chosenPath == "r":
-                        arr[pathHead[1]][pathHead[0]] += "r"
-                        nxtCell = [pathHead[0]+1,pathHead[1]]
+                            # including refrence to parent
+                            arr[pathHead[1]][pathHead[0]-1] += "r"
+                        elif chosenPath == "r":
+                            arr[pathHead[1]][pathHead[0]] += "r"
+                            nxtCell = [pathHead[0]+1,pathHead[1]]
 
-                        # including refrence to parent
-                        arr[pathHead[1]][pathHead[0]+1] += "l"
-                    elif chosenPath == "t":
-                        arr[pathHead[1]][pathHead[0]] += "t"
-                        nxtCell = [pathHead[0],pathHead[1]-1]
+                            # including refrence to parent
+                            arr[pathHead[1]][pathHead[0]+1] += "l"
+                        elif chosenPath == "t":
+                            arr[pathHead[1]][pathHead[0]] += "t"
+                            nxtCell = [pathHead[0],pathHead[1]-1]
 
-                        # including refrence to parent
-                        arr[pathHead[1]-1][pathHead[0]] += "b"
-                    else:
-                        arr[pathHead[1]][pathHead[0]] += "b"
-                        nxtCell = [pathHead[0],pathHead[1]+1]
+                            # including refrence to parent
+                            arr[pathHead[1]-1][pathHead[0]] += "b"
+                        else:
+                            arr[pathHead[1]][pathHead[0]] += "b"
+                            nxtCell = [pathHead[0],pathHead[1]+1]
 
-                        # including refrence to parent
-                        arr[pathHead[1]+1][pathHead[0]] += "t"
-                    # the nextcell now is marked both as visited
-                    visited.append(nxtCell)
+                            # including refrence to parent
+                            arr[pathHead[1]+1][pathHead[0]] += "t"
+                        # the nextcell now is marked both as visited
+                        visited.append(nxtCell)
 
-                    # current path is retained as a new path
-                    tempPath = s.copy()
-                    newSrc.append(tempPath)
+                        # current path is retained as a new path
+                        tempPath = s.copy()
+                        newSrc.append(tempPath)
 
-                    # nxtCell is added to the original path
-                    s.append(nxtCell)
+                        # nxtCell is added to the original path
+                        s.append(nxtCell)
             # if due to marking of nxtCell as visited causes any src to die it will automatically return as 0 paths
             # available to pass through thus 'if' struct will skip it automatically
             # still in order to clean up all the waste sources after all sources are checked purification shall be done
             # if this does not happen the while loop wont be able to end
         src.extend(newSrc)
         newSrc.clear()
+
+        # finding the solution in src
+        for s in src:
+            if s[-1] == [mX-1,mY-1]:
+                solution = s.copy()
 
         # purify source : add true sources to newSrc then later pass it over to src back again
         for s in src:
@@ -209,7 +226,7 @@ def makeMaze(cv,mX,mY):
     # for a in arr:
     #     print(a)
     
-    return arr
+    return (arr, solution)
 
 def drawMaze(mze, mX, mY, cv):
     # normalizing mX and mY
@@ -275,6 +292,20 @@ def displayWeb(mze, mX, mY, cv):
                 Yf = 15 + 10*(y+1)
                 k = cv.create_line(Xi, Yi, Xf, Yf ,fill="blue")
 
+def drawPath(cv, mX, mY, path):
+    mX = int(mX/10)
+    mY = int(mY/10)
+
+    if len(path) > 1:
+        for i in range(1,len(path)):
+            Xi = path[i-1][0]*10 + 15
+            Yi = path[i-1][1]*10 + 15
+            Xf = path[i][0]*10 + 15
+            Yf = path[i][1]*10 + 15
+
+            k = cv.create_line(Xi, Yi, Xf, Yf, fill="green")
+
+
 mX = int(input("ENTER X dimension of maze : ")) * 10
 mY = int(input("ENTER Y dimension of maze : ")) * 10
 
@@ -286,8 +317,13 @@ root.geometry(str(mX+20)+"x"+str(mY+20))
 cv = tk.Canvas(root, width=mX+20, height=mY+20)
 cv.pack()
 
-# DRAW MAZE
-mzArr = makeMaze(cv,mX,mY)
+# MAKE MAZE
+out = makeMaze(cv,mX,mY)
+mzArr = out[0]
+sol = out[1]
+
+# DRAW SOLUTION
+#drawPath(cv,mX,mY,sol)  # remove comment to see solution
 
 # DEBUG : display web
 # displayWeb(mzArr, mX, mY, cv)
